2fb70196cb12374bad483212bba9cc065be0e484
hmh <huangminhang@fjzzhy.cn>
2022-10-17 15:40:50 +0800

    feature:菜单栏

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,60 +1,44 @@
+<!--marks: op版代码最简化-->
 <template>
   <div class="box">
     <t-menu
         v-model:expanded="expanded"
         :theme="store.state.theme"
-        default-value="3-2"
+        default-value="dash"
         expand-mutex
         height="550px"
         :collapsed="collapsed"
     >
-      <t-submenu value="3">
-        <template #icon>
-          <t-icon name="mail"/>
-        </template>
-        <template #title>
-          <span>消息区</span>
-        </template>
-        <t-submenu value="3-1" title="二级菜单">
-          <t-menu-item value="3-1-1"> 三级菜单内容</t-menu-item>
-          <t-menu-item value="3-1-2"> 三级菜单内容</t-menu-item>
-          <t-menu-item value="3-1-3"> 三级菜单内容</t-menu-item>
-        </t-submenu>
-        <t-submenu value="3-5" title="二级菜单">
-          <t-menu-item value="3-5-1"> 三级菜单内容</t-menu-item>
-          <t-menu-item value="3-5-2"> 三级菜单内容</t-menu-item>
-          <t-menu-item value="3-5-3"> 三级菜单内容</t-menu-item>
-        </t-submenu>
-        <t-menu-item value="3-2"> 二级菜单内容</t-menu-item>
-        <t-menu-item value="3-3"> 二级菜单内容</t-menu-item>
-        <t-menu-item value="3-4"> 二级菜单内容</t-menu-item>
-      </t-submenu>
-      <t-menu-item value="user-circle">
-        <template #icon>
-          <t-icon name="user-circle"/>
-        </template>
-        个人中心
-      </t-menu-item>
-      <t-submenu value="4">
-        <template #icon>
-          <t-icon name="play-circle"/>
-        </template>
-        <template #title>
-          <span>视频区</span>
-        </template>
-        <t-menu-item value="4-1"> 二级菜单内容</t-menu-item>
-        <t-menu-item value="4-2"> 二级菜单内容</t-menu-item>
-        <t-menu-item value="4-3"> 二级菜单内容</t-menu-item>
-      </t-submenu>
-      <t-menu-item value="edit1">
-        <template #icon>
-          <t-icon name="edit-1"/>
-        </template>
-        资源编辑
-      </t-menu-item>
-      <template #operations>
-        <t-icon class="t-menu__operations-icon" name="view-list" @click="changeCollapsed"/>
+      <template #logo>
+        <img width="200" src="https://www.tencent.com/img/index/menu_logo_hover.png" alt="logo"/>
       </template>
+      <t-menu-item value="dash">
+        <template #icon>
+          <t-icon name="app"/>
+        </template>
+        面板中心
+      </t-menu-item>
+      <t-submenu value="paymanager">
+        <template #icon>
+          <t-icon name="layers"/>
+        </template>
+        <template #title>
+          <span>账单管理</span>
+        </template>
+        <t-menu-item value="3-1-1"> 我的账本</t-menu-item>
+      </t-submenu>
+      <t-menu-item value="analysis">
+        <template #icon>
+          <t-icon name="cloud"/>
+        </template>
+        账单分析
+      </t-menu-item>
+      <t-menu-item value="personalized">
+        <template #icon>
+          <t-icon name="tools"/>
+        </template>
+        个性化配置
+      </t-menu-item>
     </t-menu>
   </div>
 </template>
@@ -62,6 +46,7 @@
 <script setup>
 import {ref} from 'vue';
 import store from "../store";
+
 const collapsed = ref(false);
 const collapsed2 = ref(false);
 
